816e2cef85df73784b29b13bfdddd67101f2bd3f
Sheena Mae Delima <151040700+sheenugh@users.noreply.github.com>
2024-11-29 09:27:57 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const display = document.getElementById("display");
+
+function append_to_display(input) {
+  display.value += input;
+}
+
+function clear_display() {
+  display.value = "";
+}
+
+function     calculate() 
